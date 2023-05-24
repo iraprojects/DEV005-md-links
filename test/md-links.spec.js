f5f2efdbb883ff326f3c02348b42e8ba9d683c57
iraprojects <kristenirazu@gmail.com>
2023-05-23 22:50:23 -0600

    [Jest]Hice test de resultados a searchMD

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,36 @@
-const mdLinks = require('..');
+const {
+  searchMD, validate, readFile, getStatus,
+} = require('../index');
 
-describe('mdLinks', () => {
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('searchMD', () => {
+  it('should be a function', () => {
+    expect(typeof searchMD).toBe('function');
+  });
+  it('should return md files', () => {
+    const route = 'C:/Users/kris_/Desktop/PruebasMD';
+    const result = [
+      'C:\\Users\\kris_\\Desktop\\PruebasMD\\otro\\holu.md',
+      'C:\\Users\\kris_\\Desktop\\PruebasMD\\otro\\more files\\google.md',
+      'C:\\Users\\kris_\\Desktop\\PruebasMD\\README.md',
+    ];
+    expect(searchMD(route)).toEqual(result);
+  });
+});
+
+describe('validate', () => {
+  it('should be a function', () => {
+    expect(typeof validate).toBe('function');
+  });
+});
+
+describe('readFile', () => {
+  it('should be a function', () => {
+    expect(typeof readFile).toBe('function');
+  });
+});
+
+describe('getStatus', () => {
+  it('should be a function', () => {
+    expect(typeof getStatus).toBe('function');
   });
 });
